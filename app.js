957e70ca2050687f414307e32f40873d2686657f
ktpm489 <hoangvu@bacoor.co>
2021-05-28 17:28:18 +0700

    Add post notification

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+require('dotenv').config()
 // const url = 'mongodb://localhost/AlienDBex'
 const url = 'mongodb+srv://demo1234:P7x98AGGptztPYtj@cluster0.v7jwx.mongodb.net/tikitechsdk?retryWrites=true&w=majority'
 
 const app = express()
+
 
 mongoose.connect(url, { useNewUrlParser: true } )
 const con = mongoose.connection
@@ -16,8 +18,10 @@ app.use(express.json())
 
 const sdkRouter = require('./routes/sdk')
 const itemRouter = require('./routes/item')
+const messageRouter = require('./routes/message')
 app.use('/sdk', sdkRouter)
 app.use('/itemSdk', itemRouter)
+app.use('/message', messageRouter)
 app.use((req, res, next) => {
     const err = new Error('Not Found');
     err.status = 404;
@@ -31,6 +35,7 @@ app.use((err, req, res, next) => {
     res.render('error');
 });
 var port = process.env.PORT || 3000;
+console.log('process.env.ONESIGNAL_APP_ID', process.env.ONESIGNAL_APP_ID)
 app.listen(port, () => {
     console.log('Server started !!!')
 })
